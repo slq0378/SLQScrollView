ef586734ade78c7a62a3230f96faf512e984ec16
songlq <songlq@Niu-B.local>
2015-10-27 10:48:55 +0800

    Initial Commit

---
+++ SLQScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLQScrollView
+//
+//  Created by songlq on 15/10/27.
+//  Copyright (c) 2015年 songlq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SLQScrollViewTests/SLQScrollViewTests.m
@@ -0,0 +1,40 @@
+//
+//  SLQScrollViewTests.m
+//  SLQScrollViewTests
+//
+//  Created by songlq on 15/10/27.
+//  Copyright (c) 2015年 songlq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SLQScrollViewTests : XCTestCase
+
+@end
+
+@implementation SLQScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
